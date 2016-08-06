a28d2a82395baa5d12d7bc18c70c824c905917ff
Natasha Jadavji <natasha@jadavji.ca>
2016-08-06 21:02:14 +0000

    Created user model, controller and table

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create]
 end
