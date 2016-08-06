debfb5ef7fc214b341778105429469ac0141cf81
Natasha Jadavji <natasha@jadavji.ca>
2016-08-06 23:12:59 +0000

    implemented login and signup feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20160806221001) do
-
-  create_table "language_tests", force: :cascade do |t|
-    t.string   "language"
-    t.string   "test_evaluation"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-
-ActiveRecord::Schema.define(version: 20160806213029) do
 
   create_table "assessments", force: :cascade do |t|
     t.integer  "user_id"
@@ -39,7 +30,13 @@ ActiveRecord::Schema.define(version: 20160806213029) do
     t.datetime "created_at",            null: false
     t.datetime "updated_at",            null: false
     t.index ["user_id"], name: "index_assessments_on_user_id"
+  end
 
+  create_table "language_tests", force: :cascade do |t|
+    t.string   "language"
+    t.string   "test_evaluation"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
   create_table "users", force: :cascade do |t|
