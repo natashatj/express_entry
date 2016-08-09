9a8ce6b71fa78830473b281747e871cd35bce778
Natasha Jadavji <natasha@jadavji.ca>
2016-08-09 17:22:25 +0000

    dropdowns for immigration page

--- app/controllers/immigration_controller.rb
+++ app/controllers/immigration_controller.rb
@@ -1,5 +1,9 @@
 class ImmigrationController < ApplicationController
   def home
     @edu_levels = EduLevel.all
+    @languages = LanguageTest.limit(2)
+    @language_tests = LanguageTest.all
+    @french_language_tests = LanguageTest.where("language = 'French'")
+    @english_language_tests = LanguageTest.where("language = 'English'")
   end
 end
--- app/models/language_test.rb
+++ app/models/language_test.rb
@@ -4,8 +4,8 @@ class LanguageTest < ApplicationRecord
 
 	def self.create_lang_eval
 		LanguageTest.create language: "French", test_evaluation: "Informal"
-		LanguageTest.create language: "French", test_evaluation: "TEFL"
 		LanguageTest.create language: "English", test_evaluation: "CELPIP"
+		LanguageTest.create language: "French", test_evaluation: "TEFL"
 		LanguageTest.create language: "English", test_evaluation: "IELTS"
 		LanguageTest.create language: "English", test_evaluation: "Informal"
 	
