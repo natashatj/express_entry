8968adf06d2e787caf3a7824f28b24e8c35705ab
covrigel <alexandra.nemes1986@gmail.com>
2016-08-10 20:39:57 +0000

    undo on summary_page controller

--- app/controllers/summary_page_controller.rb
+++
@@ -1,2 +0,0 @@
-class SummaryPageController < ApplicationController
-end
--- test/controllers/summary_page_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class SummaryPageControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
