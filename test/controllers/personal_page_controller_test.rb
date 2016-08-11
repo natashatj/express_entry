13b55c2965402af03a0a0830a6f656732b74e341
Natasha Jadavji <natasha@jadavji.ca>
2016-08-11 19:25:42 +0000

    refactored stuff

--- test/controllers/personal_page_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class PersonalPageControllerTest < ActionDispatch::IntegrationTest
-  test "should get submit" do
-    get personal_page_submit_url
-    assert_response :success
-  end
-
-end
