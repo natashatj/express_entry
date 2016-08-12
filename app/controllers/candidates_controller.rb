c9cd6a43e05edb21ac0fa6b86eeb5cb2162a8799
Natasha Jadavji <natasha@jadavji.ca>
2016-08-12 19:25:39 +0000

    refactored and indented code

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,10 +1,6 @@
 class CandidatesController < ApplicationController
  
-# TODO where to route to for create on save
   def new
-
-    # p params
-
     @edu_levels = EduLevel.all
     @languages = LanguageTest.limit(2)
     @language_tests = LanguageTest.all
@@ -18,15 +14,9 @@ class CandidatesController < ApplicationController
   end
 
   def create
-    # @candidate = Candidate.new(candidate_params)
-    # @assessment = Assessment.new(user_id: session[:user_id])
-    # assessment 
     @id = params["assessment_id"]
     @assessment = Assessment.find(@id)
     @candidate = @assessment.candidates.new(candidate_params)
-    @candidate.edu_level_id = 1
-    # @candidate.country_id = 1
-    binding.pry
     if @candidate.save
       puts "Yay"
     else
@@ -38,28 +28,28 @@ class CandidatesController < ApplicationController
 
   def candidate_params
     params.require(:candidate).permit(:assessment_id, 
-                                      :edu_levels_id, 
-                                      :countries_id, 
-                                      :first_name, 
-                                      :middle_name, 
-                                      :last_name, 
-                                      :dob, 
-                                      :occupation, 
-                                      :funds,
-                                      :bac_is_pro?, 
-                                      :kids, 
-                                      :frg_work_xp_none, 
-                                      :frg_work_xp_one_or_two, 
-                                      :frg_work_xp_three_or_more, 
-                                      :cdn_xp_visited, 
-                                      :cdn_xp_fr_en_studies, 
-                                      :cdn_xp_ps_greater_than_one, 
-                                      :cdn_xp_work_greater_than_one, 
-                                      :cdn_xp_work_permit_paid, 
-                                      :cdn_xp_study_perm_dli, 
-                                      :cdn_xp_none, 
-                                      :cdn_xp_one_yr, 
-                                      :cdn_xp_one_or_more) 
+      :edu_levels_id, 
+      :countries_id, 
+      :first_name, 
+      :middle_name, 
+      :last_name, 
+      :dob, 
+      :occupation, 
+      :funds,
+      :bac_is_pro?, 
+      :kids, 
+      :frg_work_xp_none, 
+      :frg_work_xp_one_or_two, 
+      :frg_work_xp_three_or_more, 
+      :cdn_xp_visited, 
+      :cdn_xp_fr_en_studies, 
+      :cdn_xp_ps_greater_than_one, 
+      :cdn_xp_work_greater_than_one, 
+      :cdn_xp_work_permit_paid, 
+      :cdn_xp_study_perm_dli, 
+      :cdn_xp_none, 
+      :cdn_xp_one_yr, 
+      :cdn_xp_one_or_more) 
   end
 end
 
