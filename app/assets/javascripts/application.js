2d84fb8653390d38fe32601d538d7115219fba97
covrigel <alexandra.nemes1986@gmail.com>
2016-08-13 17:17:48 +0000

    added google fonts and moved video js in application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -141,6 +141,12 @@ $(function(){
   });
 });
 
-
+// video on main page
+var w = $(window).width(), h = $(window).height()  
+$('#video-block video').css({height: ''+h+'',width: ''+w+''});
+$(window).resize(function(){
+  var w = $(window).width(), h = $(window).height()  
+  $('#video-block video').css({height: ''+h+'',width: ''+w+''});
+});
 // // // = require_tree .
 
