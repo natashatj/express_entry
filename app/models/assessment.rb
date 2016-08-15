c41f395de49e886faa99fbd47cc0d72ec6b77163
covrigel <alexandra.nemes1986@gmail.com>
2016-08-15 00:11:03 +0000

    added nested forms - does not save yet

--- app/models/assessment.rb
+++ app/models/assessment.rb
@@ -1,6 +1,7 @@
 class Assessment < ApplicationRecord
   belongs_to :user
   has_many :candidates
+  accepts_nested_attributes_for :candidates
 
   def second(candidates)
     candidates.length <= 1 ? nil : candidates[1]
