36b9aa791b0fe3f615ef88fb531185d06d41f1eb
FioFiyo <fioarlego1@msn.com>
2016-08-16 00:44:27 +0000

    added candidate 2 in create action and started setting up db info in the summary page

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -18,15 +18,60 @@ class CandidatesController < ApplicationController
 
     @id = params["assessment_id"]
     @assessment = Assessment.find(@id)
+
     @candidate = @assessment.candidates.new(candidate_params)
-    
-    if @candidate.save
+
+    @candidate_2 = @assessment.candidates.new(
+      edu_level_id: params[:edu_level_id],
+      dob: params[:dob],
+      country_id: params[:country_id], 
+      first_name: params[:first_name], 
+      middle_name: params[:middle_name], 
+      last_name: params[:last_name],  
+      occupation: params[:occupation], 
+      funds: params[:funds],
+      bac_is_pro?: params[:bac_is_pro?], 
+      kids: params[:kids], 
+      is_married: params[:is_married],
+      frg_work_xp_none: params[:frg_work_xp_none], 
+      frg_work_xp_one_or_two: params[:frg_work_xp_one_or_two], 
+      frg_work_xp_three_or_more: params[:frg_work_xp_three_or_more], 
+      cdn_xp_visited: params[:cdn_xp_visited], 
+      cdn_xp_fr_en_studies: params[:cdn_xp_fr_en_studies], 
+      cdn_xp_ps_greater_than_one: params[:cdn_xp_ps_greater_than_one], 
+      cdn_xp_work_greater_than_one: params[:cdn_xp_work_greater_than_one], 
+      cdn_xp_work_permit_paid: params[:cdn_xp_work_permit_paid], 
+      cdn_xp_study_perm_dli: params[:cdn_xp_study_perm_dli], 
+      cdn_xp_none: params[:cdn_xp_none], 
+      cdn_xp_years: params[:cdn_xp_years]
+
+      )
+
+    if @candidate.save 
       @candidate_age = calculate_age(@candidate.dob)
       @candidate_age_points = calculate_points_for_age(@candidate_age)
       @candidate_edu_points = calculate_points_for_edu(@candidate.edu_level_id)
 
      
       @candidate_cdn_work_points = cdn_work(@candidate.cdn_xp_years)
+
+      @candidate_adapt_cdn_work_ed = adaptability_ed_cdn_xp
+      @candidate_adapt_cdn_work_frg_work =adaptability_frg_xp_and_cdn_xp
+       # binding.pry
+      
+      # @candidate_first_language = calculate_points_for_language(@candidate)
+      
+    else
+      render :new
+    end
+
+     if @candidate_2.save 
+      @candidate_2_age = calculate_age(@candidate_2.dob)
+      @candidate_2_age_points = calculate_points_for_age_2(@candidate_2_age)
+      @candidate_2_edu_points = calculate_points_for_edu_2(@candidate_2.edu_level_id)
+
+     
+      @candidate_2_cdn_work_points = cdn_work_2(@candidate_2.cdn_xp_years)
 
       @candidate_adapt_cdn_work_ed = adaptability_ed_cdn_xp
       @candidate_adapt_cdn_work_frg_work =adaptability_frg_xp_and_cdn_xp
@@ -142,30 +187,63 @@ class CandidatesController < ApplicationController
         @candidate.points += single_age_points
       end
     end
+  def calculate_points_for_age_2(age)
+    @age_points_married_2 = {
+          20 => 100,
+          21 => 100,
+          22 => 100,
+          23 => 100,
+          24 => 100,
+          25 => 100,
+          26 => 100,
+          27 => 100,
+          28 => 100,
+          29 => 100,
+          19 => 95,
+          30 => 95,
+          18 => 90,
+          31 => 90,
+          32 => 85,
+          33 => 80,
+          34 => 75,
+          35 => 70,
+          36 => 65,
+          37 => 60,
+          38 => 55,
+          39 => 50,
+          40 => 45,
+          41 => 35,
+          42 => 25,
+          43 => 15,
+          44 => 5
+        }
+    married_age_points_2 =  @age_points_married_2.fetch(age)
+    @candidate_2.points += married_age_points_2     
+  end
 
-    def calculate_points_for_edu(edu_level)
+  def calculate_points_for_edu(edu_level)
 
-      @edu_points_married = {
-        1 => 28,
-        2 => 84,
-        3 => 91,
-        4 => 112,
-        5 => 119,
-        6 => 126,
-        7 => 140
+    @edu_points_married = {
+      1 => 28,
+      2 => 84,
+      3 => 91,
+      4 => 112,
+      5 => 119,
+      6 => 126,
+      7 => 140
 
-      }
+    }
 
-      @edu_points_single = {
-        1 => 30,
-        2 => 90,
-        3 => 98,
-        4 => 120,
-        5 => 128,
-        6 => 135,
-        7 => 150
-      }
-  
+    @edu_points_single = {
+      1 => 30,
+      2 => 90,
+      3 => 98,
+      4 => 120,
+      5 => 128,
+      6 => 135,
+      7 => 150
+    }
+
 
     if @candidate.is_married
       married_edu_points = @edu_points_married.fetch(edu_level)
@@ -175,8 +253,22 @@ class CandidatesController < ApplicationController
       @candidate.points += single_edu_points
     end
   end
-  
 
+  def calculate_points_for_edu_2(edu_level)
+      @edu_points_married_2 = {
+        1 => 28,
+        2 => 84,
+        3 => 91,
+        4 => 112,
+        5 => 119,
+        6 => 126,
+        7 => 140
+      } 
+      married_edu_points_2 = @edu_points_married_2.fetch(edu_level)
+      @candidate_2.points += married_edu_points_2
+    end
+  end
+  
     def cdn_work(cdn_work_xp)
       @cdn_work_married = {
 
@@ -196,12 +288,25 @@ class CandidatesController < ApplicationController
         5 => 80
       }
       if @candidate.is_married
-      married_cdn_work_points = @cdn_work_married.fetch(cdn_work_xp)
-      @candidate.points += married_cdn_work_points
-    elsif !@candidate.is_married
-      single_cdn_work_points = @cdn_work_single.fetch(cdn_work_xp)
-      @candidate.points += single_cdn_work_points
+        married_cdn_work_points = @cdn_work_married.fetch(cdn_work_xp)
+        @candidate.points += married_cdn_work_points
+      elsif !@candidate.is_married
+        single_cdn_work_points = @cdn_work_single.fetch(cdn_work_xp)
+        @candidate.points += single_cdn_work_points
+      end
     end
+
+    def cdn_work_2(cdn_work_xp)
+      @cdn_work_married_2 = {
+
+        1 => 35,
+        2 => 46,
+        3 => 56,
+        4 => 63,
+        5 => 70
+      }
+      married_cdn_work_points_2 = @cdn_work_married_2.fetch(cdn_work_xp)
+      @candidate_2.points += married_cdn_work_points_2
     end
 
     def adaptability_ed_cdn_xp
