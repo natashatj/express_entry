4797644c60ac43b614003c839a79a9dc999f0831
Natasha Jadavji <natasha@jadavji.ca>
2016-08-17 02:12:27 +0000

    added form effect classes to form

---
+++ app/assets/javascripts/form_effects.js
@@ -0,0 +1,8 @@
+$(function() {
+  $('.form-effect').mouseenter(function() {
+    $(this).css('color', 'red');
+  });
+  $('.form-effect').mouseleave(function() {
+    $(this).css('color', 'white');
+  });
+});
